38ae0fd3716c2a771cea32fb5a0bfb7a5a7a6326
Benjamim Amaral <75807261+synthpr0wlv1p3r-hacker@users.noreply.github.com>
2026-04-09 20:41:41 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
+string characterName = "Tom";
+int    characterAge = 50;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << " There was once a man named " << characterName << endl;
+    cout << " He was "  << characterAge <<  " years old " << endl;
+    cout << " He liked the name " << characterName << endl;
+    cout << " But he did not like being " << characterAge << endl;
+
+
+
     return 0;
 }
